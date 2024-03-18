980c071b8b7f650373921ae63ba32bc3b8f7515c
cao48 <cao48@illinois.edu>
2024-03-18 00:11:30 -0500

    rtc.h upload

---
+++ student-distrib/rtc.h
@@ -0,0 +1,29 @@
+#ifndef _RTC_H
+#define _RTC_H
+
+#include "types.h"
+#include "lib.h"
+#include "i8259.h"
+
+#define RTC_COMMAND     0x70
+#define RTC_DATA        0x71
+
+#define REG_A           0x8A
+#define REG_B           0x8B
+#define REG_C           0x0C
+
+#define RTC_IRQ         8
+#define RTC_FREQ        1024
+#define RTC_RATE_2      0x0F
+
+
+void rtc_init();
+void rtc_handler();
+int32_t rtc_open(const uint8_t* filename);
+int32_t rtc_close(int32_t fd);
+int32_t rtc_write(int32_t fd, const void* buf, int32_t nbytes);
+int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes);
+int32_t rtc_set_rate(int32_t rate);
+
+
+#endif
