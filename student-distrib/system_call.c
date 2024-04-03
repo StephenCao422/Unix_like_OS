2e8b677864f1571a345ee9738721dbe551b5a75c
RubinDu <you@example.comrd25@illinois.edu>
2024-04-02 20:17:58 -0500

    Completed execute

--- student-distrib/system_call.c
+++ student-distrib/system_call.c
@@ -2,6 +2,7 @@
 #include "lib.h"
 #include "paging.h"
 #include "filesys.h"
+#include "terminal.h"
 
 //TODO CP2
 
@@ -15,11 +16,12 @@ int32_t halt(uint8_t status){
 }
 
 int32_t execute(const uint8_t* command){
-    int i;
+    int i, pid;
     uint8_t* filename[READBUF_SIZE];
     uint8_t* args[READBUF_SIZE];
     dentry_t exec_dentry;
-    uint32_t magic_check;
+    uint32_t magic_check, eip;
+    pcb_t* pcb;
 
     memset(filename, 0, READBUF_SIZE);
     memset(args, 0, READBUF_SIZE);
@@ -55,7 +57,7 @@ int32_t execute(const uint8_t* command){
     
     //Executable check
     if (read_dentry_by_name(filename, &exec_dentry) == -1
-        ||read_data(exec_dentry.inode_num, 0, &magic_check, MAGIC_SIZE) == -1
+        ||read_data(exec_dentry.inode_num, 0, (uint8_t*)&magic_check, MAGIC_SIZE) == -1
         ||magic_check != MAGIC_NUM)
         return -1;
 
@@ -66,20 +68,59 @@ int32_t execute(const uint8_t* command){
 
     if (i==MAX_TASKS)
         return -1;
-    
-    GET_PCB(i)->pid = i;
-    GET_PCB(i)->present = 1;
-    
+
+    pid=i;
     page_directory[USER_ENTRY].MB.present=1;
     page_directory[USER_ENTRY].MB.user_supervisor=1;
-    page_directory[USER_ENTRY].val|=0x400000+i*0x400000;
+    page_directory[USER_ENTRY].val|=0x400000+pid*0x400000;
+
+    asm volatile (              //Flush TLB
+        "movl %%cr3, %%eax;"
+        "movl %%eax, %%cr3;"
+        :
+        :
+        : "%eax"
+    );
 
     //User-level Program Loader 
+    read_data(exec_dentry.inode_num, 0, (uint8_t*)USER_CODE, USER_CODE_LIMIT);
+
     //Create Process Control Block
+    pcb=GET_PCB(pid);
+    if (pid)
+        pcb->parent_pcb= current_pcb();
+    pcb->present=1;
+    pcb->pid=i;
+
+    memset(pcb->fd, 0, sizeof(file_descriptor_t)*MAX_FILES);
+    
+    pcb->fd[0].file_ops=&stdin_op;
+    pcb->fd[0].flags=1;
+    pcb->fd[1].file_ops=&stdout_op;
+    pcb->fd[1].flags=1;
+
+    for (i = 2; i < MAX_FILES; i++)
+        pcb->fd[i].file_ops=&null_op;
+
+    memcpy(pcb->args, args, READBUF_SIZE);
     //Context Switch:
+    tss.esp0=KSTACK_START-KSTACK_SIZE*pid-4;
+    read_data(exec_dentry.inode_num, 24, (uint8_t*)&eip, 4);
+
     //Create its own context switch stack
-    //IRET
-    return -1;
+    asm volatile (
+        "movw %0, %ds;"
+        "pushl %0;"
+        "pushl %1;"
+        "pushfl;"
+        "pushl %2;"
+        "pushl %3;"
+        "iret;"
+        :
+        :"r"((uint32_t)USER_DS), "r"((uint32_t)USER_STACK), "r"((uint32_t)USER_CS), "r"(eip)
+        :"memory"
+    );
+    return 0;
 }
 
 int32_t read(int32_t fd, void* buf, int32_t nbytes){
@@ -114,6 +155,22 @@ int32_t sigreturn(void){
     return -1;
 }
 
+int32_t null_read(int32_t fd, void* buf, int32_t nbytes){
+    return -1;
+}
+
+int32_t null_write(int32_t fd, const void* buf, int32_t nbytes){
+    return -1;
+}
+
+int32_t null_open(const uint8_t* filename){
+    return -1;
+}
+
+int32_t null_close(int32_t fd){
+    return -1;
+}
+
 pcb_t* current_pcb(){
     uint32_t esp;
     asm volatile ("                 \n\
@@ -121,6 +178,6 @@ pcb_t* current_pcb(){
             "
             : "=r"(esp)
     );
-    return (pcb_t*)((0x800000-esp)/0x2000);
+    return (pcb_t*)(esp&(KSTACK_START-KSTACK_SIZE));
 }
 
--- student-distrib/system_call.h
+++ student-distrib/system_call.h
@@ -5,23 +5,63 @@
 #include "filesys.h"
 #include "paging.h"
 #include "keyboard.h"
+#include "rtc.h"
 
 #define MAX_FILES 8
 #define MAGIC_SIZE 4
 #define MAGIC_NUM 0x7F454C46
 #define USER_ENTRY 32
 #define MAX_TASKS 2
+#define USER_CODE 0x8048000
+#define USER_CODE_LIMIT 0x3B8000 //4MB-0x48000
+#define USER_STACK 0x8400000-4
+#define KSTACK_START 0x800000
+#define KSTACK_SIZE 0x2000
 
-#define GET_PCB(pid) ((pcb_t*)(0x7FE000-0x2000*pid))
+#define GET_PCB(pid) ((pcb_t*)(KSTACK_START-KSTACK_SIZE-KSTACK_SIZE*pid))
+
+static const struct file_operations stdin_op = {
+    .open = terminal_open,
+    .read = terminal_read,
+    .write = null_write,
+    .close = terminal_close
+};
+
+static const struct file_operations stdout_op = {
+    .open = terminal_open,
+    .read = null_read,
+    .write = terminal_write,
+    .close = terminal_close
+};
+
+static const struct file_operations rtc_op = {
+    .open = rtc_open,
+    .read = rtc_read,
+    .write = rtc_write,
+    .close = rtc_close
+};
+
+static const struct file_operations file_op = {
+    .open = file_open,
+    .read = file_read,
+    .write = file_write,
+    .close = file_close
+};
+
+static const struct file_operations null_op = {
+    .open = null_open,
+    .read = null_read,
+    .write = null_write,
+    .close = null_close
+};
 
 typedef struct pcb{
-    file_descriptor_t file_arr[MAX_FILES];
+    file_descriptor_t fd[MAX_FILES];
     uint8_t present;
     uint32_t pid;
+    pcb_t* parent_pcb;
     uint32_t uesp;
     uint32_t uebp;
-    uint32_t kesp;
-    uint32_t kss;
     char args[READBUF_SIZE];
 }pcb_t;
 
@@ -38,4 +78,9 @@ int32_t vidmap(uint8_t** screen_start);
 int32_t set_handler(int32_t signum, void* handler_address);
 int32_t sigreturn(void);
 
+
+int32_t null_read(int32_t fd, void* buf, int32_t nbytes);
+int32_t null_write(int32_t fd, const void* buf, int32_t nbytes);
+int32_t null_open(const uint8_t* filename);
+int32_t null_close(int32_t fd);
 #endif
